02ec449d8527bed8b4be4c450c37d9137e9fe086
sedin <leesejinn@naver.com>
2020-06-30 21:53:36 +0900

    init: Initialize Project
    - Initialize Project
    - Make Simple JPA  Admin Page

---
+++ src/main/java/com/shopping/simpleadmin/SimpleAdminApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.simpleadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopping/simpleadmin/SimpleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.simpleadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
