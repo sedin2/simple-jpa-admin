e8ab6d6bfb5765acb60fecc3c2df05dc565d91c2
sedin <leesejinn@naver.com>
2020-07-02 22:31:39 +0900

    feat: Create UserRepository
    - Extends Jpa Repository

---
+++ src/main/java/com/shopping/simpleadmin/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.shopping.simpleadmin.repository;
+
+import com.shopping.simpleadmin.model.entitiy.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
