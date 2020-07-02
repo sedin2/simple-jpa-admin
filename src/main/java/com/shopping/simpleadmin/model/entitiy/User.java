3fee454ebc3835f81271609f5b80747c28c4d15b
sedin <leesejinn@naver.com>
2020-07-02 22:31:11 +0900

    feat: Create User Entity Class
    - User Entity

---
+++ src/main/java/com/shopping/simpleadmin/model/entitiy/User.java
@@ -0,0 +1,36 @@
+package com.shopping.simpleadmin.model.entitiy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
