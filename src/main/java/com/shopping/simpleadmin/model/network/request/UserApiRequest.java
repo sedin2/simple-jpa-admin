4ba147e4b6bb4bac93dbc9bce553b2b09d767c17
sedin <leesejinn@naver.com>
2020-07-26 23:09:07 +0900

    feat: Create UserApiRequest
    - Make UserApiRequest

---
+++ src/main/java/com/shopping/simpleadmin/model/network/request/UserApiRequest.java
@@ -0,0 +1,23 @@
+package com.shopping.simpleadmin.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserApiRequest {
+
+    private Long id;
+
+    private String account;
+
+    private String password;
+
+    private String email;
+
+    private String phoneNumber;
+}
